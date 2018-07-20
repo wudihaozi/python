a785e39800695998677304a197b890dad42b9d6f
wudihaozi <virusadmin@163.com>
2018-07-20 13:45:28 +0800

    new file

---
+++ subplot.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# two pic
+data = np.arange(100, 201)
+plt.subplot(2, 1, 1)
+plt.plot(data)
+
+data2 = np.arange(200, 301)
+plt.subplot(2, 1, 2)
+plt.plot(data2)
+
+plt.show()
+
+# one pic
+data3 = np.arange(100, 201)
+plt.subplot(211)
+plt.plot(data3)
+
+data4 = np.arange(200, 301)
+plt.plot(212)
+plt.plot(data4)
+
+plt.show()
