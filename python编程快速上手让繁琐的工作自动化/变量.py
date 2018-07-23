e3f706d49d61654033f4df14f7ae1c2697c481ad
wudihaozi <virusadmin@163.com>
2018-07-23 12:59:07 +0800

    new file

---
+++ python编程快速上手让繁琐的工作自动化/变量.py
@@ -0,0 +1,35 @@
+
+变量名规则
+只能是一个词
+只能包含字母、数字、下划线
+不能以数字开头
+变量名区分大小写
+
+潜规则
+变量以小写字母开头 dirName
+变量不用中文 name
+以驼峰形式命名 lookLikeThis
+
+
+赋值语句
+
+>>>spam = 40
+>>>spam
+40
+
+
+变量运算
+>>>spam = 4
+>>>eggs = 2
+>>>spam + eggs
+6
+
+
+当一个变量被赋予一个新值，老的数据就被遗忘了
+>>>spam = 'Hello'
+>>>spam
+'Hello'
+>>>spam = 'Goodbye'
+>>>spam
+'Goodbye'
+
