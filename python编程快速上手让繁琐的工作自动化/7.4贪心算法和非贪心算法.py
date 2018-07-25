03556b53363454243390fe412ee4b7ebaa6c4a24
wudihaozi <virusadmin@163.com>
2018-07-25 09:19:17 +0800

    new file

---
+++ python编程快速上手让繁琐的工作自动化/7.4贪心算法和非贪心算法.py
@@ -0,0 +1,10 @@
+import re
+
+greedyHaRegex = re.compile(r'(Ha){3,5}')
+mo1 = greedyHaRegex.search('HaHaHaHaHa')
+nongreedyHaRegex = re.compile(r'(Ha){3,5}?')
+mo2 = nongreedyHaRegex.search('HaHaHaHaHa')
+print(mo1.group())
+# HaHaHaHaHa
+print(mo2.group())
+# HaHaHa
